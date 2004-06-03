347bf4a1c8b733cb2e844ff1ec282e3782e9eea7
Jim Fulton <jim@zope.com>
2004-06-03 19:47:08 +0000

    Ripped out Persistent code support
    It's not ready for production.  This includes
    persistent modules, interfaces, and schema.

---
+++ __init__.py
@@ -0,0 +1,89 @@
+##############################################################################
+#
+# Copyright (c) 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Code about interfaces.
+
+This module contains code for interfaces in persistent modules.
+
+$Id$
+"""
+from zope.proxy import removeAllProxies
+
+from zope.interface.declarations import providedBy
+
+def queryType(object, interface):
+    """Returns the object's interface which implements interface.
+
+    >>> from zope.app.content.interfaces import IContentType
+    >>> from zope.interface import Interface, implements, directlyProvides
+    >>> class I(Interface):
+    ...     pass
+    >>> class J(Interface):
+    ...     pass
+    >>> directlyProvides(I, IContentType)
+    >>> class C:
+    ...     implements(I)
+    >>> class D:
+    ...     implements(J,I)
+    >>> obj = C()
+    >>> c1_ctype = queryType(obj, IContentType)
+    >>> c1_ctype.__name__
+    'I'
+    >>> class I1(I):
+    ...     pass
+    >>> class I2(I1):
+    ...     pass
+    >>> class I3(Interface):
+    ...     pass
+    >>> class C1:
+    ...     implements(I1)
+    >>> obj1 = C1()
+    >>> c1_ctype = queryType(obj1, IContentType)
+    >>> c1_ctype.__name__
+    'I'
+    >>> class C2:
+    ...     implements(I2)
+    >>> obj2 = C2()
+    >>> c2_ctype = queryType(obj2, IContentType)
+    >>> c2_ctype.__name__
+    'I'
+
+    >>> class C3:
+    ...     implements(I3)
+    >>> obj3 = C3()
+
+    If Interface doesn't provide IContentType, queryType returns None.
+    
+    >>> c3_ctype = queryType(obj3, IContentType)
+    >>> c3_ctype
+    >>> c3_ctype is None
+    True
+    >>> class I4(I):
+    ...     pass
+    >>> directlyProvides(I4, IContentType)
+    >>> class C4:
+    ...     implements(I4)
+    >>> obj4 = C4()
+    >>> c4_ctype = queryType(obj4, IContentType)
+    >>> c4_ctype.__name__
+    'I4'
+
+    """
+    object = removeAllProxies(object)
+    
+    object_iro = providedBy(object).__iro__
+    for iface in object_iro:
+        if interface.providedBy(iface):
+            return iface
+        
+    return None
