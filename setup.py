64f6a09689a392f4bba848bc4b0b93b131cd0759
Bernd Dorn <bernddorn@gmail.com>
2007-03-24 19:03:26 +0000

    skel

---
+++ setup.py
@@ -0,0 +1,41 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.interface package
+
+$Id$
+"""
+
+import os
+
+from setuptools import setup, find_packages, Extension
+
+setup(name='zope.app.interface',
+      version='3.4dev',
+      url='http://svn.zope.org/zope.app.interface',
+      license='ZPL 2.1',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description='The Security framework provides a generic mechanism '
+                       'to implement security policies on Python objects.',
+      
+	  packages=find_packages('src'),
+	  package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      install_requires=['setuptools',
+                       ],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
