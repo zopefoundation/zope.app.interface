7b1747f45f8c8705ec8dcb16cb69869232c62d97
Michael Howitz <mh@gocept.com>
2010-09-20 07:27:11 +0000

    merged branch icemac-queryType to inverse dependency on queryType to zope.app.content

--- src/zope/app/interface/__init__.py
+++ src/zope/app/interface/__init__.py
@@ -22,19 +22,20 @@ __docformat__ = 'restructuredtext'
 
 from persistent import Persistent
 from zodbcode.patch import registerWrapper, Wrapper, NameFinder
-
 from zope.interface.interface import InterfaceClass
 from zope.interface import Interface
-from zope.security.proxy import removeSecurityProxy
-
 from wref import FlexibleWeakKeyDictionary
+
+# BBB import
+from zope.app.content import queryType
+
 
 class PersistentInterfaceClass(Persistent, InterfaceClass):
 
     def __init__(self, *args, **kw):
         Persistent.__init__(self)
         InterfaceClass.__init__(self, *args, **kw)
-        
+
         self.dependents = FlexibleWeakKeyDictionary()
 
 # PersistentInterface is equivalent to the zope.interface.Interface object
@@ -69,74 +70,4 @@ NameFinder.types[InterfaceClass] = True
 NameFinder.classTypes[PersistentInterfaceClass] = True
 NameFinder.types[PersistentInterfaceClass] = True
 
-from zope.interface.declarations import providedBy
 
-def queryType(object, interface):
-    """Returns the object's interface which implements interface.
-
-    >>> from zope.interface import Interface
-    >>> class IContentType(Interface):
-    ...    pass
-    >>> from zope.interface import Interface, implements, directlyProvides
-    >>> class I(Interface):
-    ...     pass
-    >>> class J(Interface):
-    ...     pass
-    >>> directlyProvides(I, IContentType)
-    >>> class C(object):
-    ...     implements(I)
-    >>> class D(object):
-    ...     implements(J,I)
-    >>> obj = C()
-    >>> c1_ctype = queryType(obj, IContentType)
-    >>> c1_ctype.__name__
-    'I'
-    >>> class I1(I):
-    ...     pass
-    >>> class I2(I1):
-    ...     pass
-    >>> class I3(Interface):
-    ...     pass
-    >>> class C1(object):
-    ...     implements(I1)
-    >>> obj1 = C1()
-    >>> c1_ctype = queryType(obj1, IContentType)
-    >>> c1_ctype.__name__
-    'I'
-    >>> class C2(object):
-    ...     implements(I2)
-    >>> obj2 = C2()
-    >>> c2_ctype = queryType(obj2, IContentType)
-    >>> c2_ctype.__name__
-    'I'
-
-    >>> class C3(object):
-    ...     implements(I3)
-    >>> obj3 = C3()
-
-    If Interface doesn't provide `IContentType`, `queryType` returns ``None``.
-
-    >>> c3_ctype = queryType(obj3, IContentType)
-    >>> c3_ctype
-    >>> c3_ctype is None
-    True
-    >>> class I4(I):
-    ...     pass
-    >>> directlyProvides(I4, IContentType)
-    >>> class C4(object):
-    ...     implements(I4)
-    >>> obj4 = C4()
-    >>> c4_ctype = queryType(obj4, IContentType)
-    >>> c4_ctype.__name__
-    'I4'
-
-    """
-    # Remove the security proxy, so that we can introspect the type of the
-    # object's interfaces.
-    naked = removeSecurityProxy(object)
-    object_iro = providedBy(naked).__iro__
-    for iface in object_iro:
-        if interface.providedBy(iface):
-            return iface
-
-    return None
